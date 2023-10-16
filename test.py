ce1b486224be162e703c126972d0b1e18e775376
大傻逼 <m.hawes93@gmail.com>
2023-10-16 17:34:35 +0100

    updated jupyter

---
+++ test.py
@@ -0,0 +1,6 @@
+ages = [30, 25, 22, 20, 18, 15, 12, 10, 8, 6, 4, 2, 0, 25]
+
+# get index of 25, there is more than 1 
+# print(ages.index(25)) # 1
+# print(ages.index(25, 2)) # 13
+# print(ages.index(25, 2, 13)) # 13
