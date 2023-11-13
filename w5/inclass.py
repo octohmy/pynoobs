02ebe5f14de5b2c75f3f3ee4b3b620d0ce445443
大傻逼 <m.hawes93@gmail.com>
2023-11-13 17:48:31 +0000

    more text

--- w5/inclass.py
+++ w5/inclass.py
@@ -38,6 +38,40 @@
 
 # anyway, classes use camelCase
 
+# let's make a human class and hardcode some values
+
+class Human:
+    name = "Michael"
+    age = 21
+    pros_and_cons = ["good at drinking tea", "bad at sleeping early"]
+    
+    # you can use self within the class to refer to the object itself, see example below
+    
+    # let's write a function within this class
+    def speak(self):
+        print("Hello, my name is", self.name) 
+        # by using self.name we can use the name variable within the class
+    
+human = Human()
+
+human.speak()
+
+human.name = "Daniel"
+print(human.name)
+
+human.speak()
+
+# let's step this up a gear with a constructor
+# a constructor is a function that is called when the object is created
+# it's called __init__ and it's a special function
+# whenever the object is created, the __init__ function is called
+
+# in the example below of class 'Car' whenever we use the class it'll automatically call the __init__ function, and we can pass in the values for the attributes, to give it a colour, make, model, year, etc.
+
+# if you don't pass values in, it'll throw an error
+    
+# let's make a class called 'Car' with the __init__ function
+
 class Car:
     def __init__ (self, colour, make, model, year):
         self.colour = colour
@@ -47,4 +81,24 @@ class Car:
         
     def drive(self):
         print("Vroom vroom, I'm in me mum's car")
+        print(f"Vroom vroom, I'm in me mum's {self.colour} {self.make}")
+        
+    def honk_horn(self):
+        print("beep beep")
+        print(f"beep beep, I'm a {self.make}, get out me way")
+        
 
+# let's make a car object
+# we have to pass in the values for the attributes
+
+lambo_car = Car("red", "Lamborghini", "Aventador", 2019)
+ferrari_car = Car("yellow", "Ferrari", "F40", 1992)
+
+# let's print the attributes of the car
+print(f"All the attributes of the lambo are:\n{lambo_car.__dict__}")
+print(f"The colour of the lambo is {lambo_car.colour}")
+print(f"The make of the lambo is {lambo_car.make}")
+
+# i want to honk the horn of the lambo and drive it
+lambo_car.honk_horn()
+lambo_car.drive()
