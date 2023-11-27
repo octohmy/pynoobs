799cffa3cc3d55ebec1982377b17f02dbef2c537
大傻逼 <m.hawes93@gmail.com>
2023-11-27 05:55:52 +0000

    burger shop

--- w6/no_satisfaction.py
+++ w6/no_satisfaction.py
@@ -12,13 +12,27 @@
 
 # while i'm riding round the world and i'm doing this and i'm signing that 🥁🎸👅
 
+
+# mini challenge 1: 
 # write a while loop that prints out the lyrics to the song
 # ask the user if they're satisfied
 # only stop singing the song when the user is satisfied
 
 
+# mini challenge 2:
+# write a for loop to print the lyrics of yellow submarine 3 times
+# extra challenge: print the lyrics of yellow submarine 3 times, but each time
+# it prints, it prints a different colour submarine
 
 
+
+
+
+
+# mini challenge 3:
+# write a class called 'Burger' with the __init__ function
+# it should have 2 attributes: 'name' and 'price'
+# it should have 2 methods: 'get_name' and 'get_price'
 
 
 
@@ -171,5 +185,60 @@ while not satisfied:
     print("I can't get no, I can't get no")
     print()
     satisfied = input("Satisfied? (y/n): ").lower() == "y"
-    print("Keep going")
     print()
+
+
+
+
+
+
+
+
+
+
+
+
+yellow_submarine_chorus = [
+    "We all live in a yellow submarine",
+    "Yellow submarine, yellow submarine"
+]
+
+number_of_choruses = 3  # How many times we want to sing the chorus
+
+for _ in range(number_of_choruses):
+    for line in yellow_submarine_chorus:
+        print(line)
+        time.sleep(2)  # Pause for effect
+    print()  # Add a new line after each chorus
+
+
+
+
+
+
+
+
+
+
+
+
+class Burger:
+    def __init__ (self, name, price):
+        self.name = name
+        self.price = price
+        
+    def get_name(self):
+        message = f"This burger is called {self.name}"
+        print(message)
+        return message
+    
+    def get_description(self):
+        message = f"This burger is called {self.name} and it costs ${self.price}"
+        print(message)
+        return message
+
+    def get_price(self):
+        print(f"This burger costs ${self.price}")
+        return self.price
+    
+Burger("Big Mac", 5.99).get_description()
