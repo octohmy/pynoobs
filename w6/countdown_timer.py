6f421c018bad3854341d8fb1349562036e8d141f
大傻逼 <m.hawes93@gmail.com>
2023-11-27 14:15:11 +0000

    minor tweaks

---
+++ w6/countdown_timer.py
@@ -0,0 +1,21 @@
+import time
+from art import *
+
+
+def countdown_timer(minutes = 1, seconds = 10):
+    # convert minutes to seconds and add to seconds
+    seconds += minutes * 60
+    # loop through the seconds
+    for i in range(seconds, 0, -1):
+        # print the time left with x minutes and x seconds
+        print(f"Time remaining: {i // 60} minutes, {i % 60} seconds")
+    
+        # wait one second
+        time.sleep(1)
+        # now that one second has passed, the loop will run again
+        tprint("pyNOOBS", font="alligator", chr_ignore=True)
+    
+    # when the loop is done, print "Time's up!"
+    print ("Time's upppp!")
+
+countdown_timer()
